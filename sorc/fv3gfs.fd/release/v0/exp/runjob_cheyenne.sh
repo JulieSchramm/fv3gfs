02086ae2b6d51c881b9bd72880337ca4d1701583
Julie Schramm <schramm@ucar.edu>
2018-05-03 10:56:43 -0600

    - Modifications to run fv3gfs V1 with a script: - Update runjob_cheyenne.sh from V0 version to V1 - Update diag_table and field_table to V1 - Update exglobal_fcst_nemsfv3gfs.sh to V1, this   sets up the namelist and modified/copies tables to INPUT dir

--- sorc/fv3gfs.fd/release/v0/exp/runjob_cheyenne.sh
+++ sorc/fv3gfs.fd/release/v0/exp/runjob_cheyenne.sh
@@ -5,8 +5,8 @@
 #PBS -A P48503002
 #PBS -q economy
 ###PBS -l select=48:ncpus=36:mpiprocs=36
-### Request 12 nodes, each with 36 MPI tasks, and one thread
-#PBS -l select=24:ncpus=36:mpiprocs=36:ompthreads=1
+### Request 12 nodes, each with 36 MPI tasks
+#PBS -l select=8:ncpus=24:mpiprocs=24
 #PBS -l walltime=00:40:00
 
 set -x
@@ -43,15 +43,17 @@ export CASE=C192                  #resolution, C96, C384 or C768
 export CDATE=2016021000           #initial condition dates  2016021000 2017081712 
                                   #                         2018022800  2018032212
 
-#JLS add ESMF shared library path to LD_LIBRARY_PATH
+# Add ESMF shared library path to LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/glade/p/work/heinzell/fv3/esmf-7.1.0_bs52/intel-17.0.1/mpt-2.15f/lib/libO/Linux.intel.64.mpi.default
 
-export BASE_DATA=/glade/p/ral/jnt/GMTB/FV3GFS_V1_RELEASE ;# data directory
+export BASE_DATA=/glade/p/ral/jnt/GMTB/FV3GFS_V1_RELEASE # data directory
 export FIX_FV3=$BASE_DATA/fix/fix_fv3                 #model fixed fields
 export IC_DIR=$BASE_DATA/ICs                          #forecast initial conditions 
 
 # temporary running directory
 export DATA=/glade/scratch/$LOGNAME/${CASE}${PSLOT}${CDATE}     
+
+# This will clean out your $DATA directory
 if [ -d $DATA ]; then rm -rf $DATA ; fi
 
 # directory to save output
@@ -62,15 +64,14 @@ FV3DIR=${1:-`pwd`/../../..}
 FV3DIR_RELEASE=${1:-`pwd`/..}
 export FCSTEXECDIR=$FV3DIR/NEMS/exe
 
-export FHMAX=48                                       #maximum forecast hours
-export FHOUT=3                                        #forecast output frequency in hours
+export FHMAX=24                                       #maximum forecast hours
+export FHOUT=6                                        #forecast output frequency in hours
 #---------------------------------------------------------
 #---------------------------------------------------------
 case $CASE in
   C96)  export DELTIM=1800; export layout_x=6; export layout_y=8;  export NODES=24; 
         export master_grid=1deg;   export REMAP_TASKS=48 ;;
-#JLS  C192) export DELTIM=900 ; export layout_x=12; export layout_y=16;  export NODES=96;
-  C192) export DELTIM=900 ; export layout_x=8; export layout_y=9;  export NODES=24;
+  C192) export DELTIM=450 ; export layout_x=4; export layout_y=6;  export NODES=8;
         export master_grid=0p5deg; export REMAP_TASKS=72 ;;
   C384) export DELTIM=450 ; export layout_x=12; export layout_y=16;  export NODES=96;
         export master_grid=0p5deg; export REMAP_TASKS=96 ;;
@@ -84,10 +85,10 @@ export PARM_FV3DIAG=$FV3DIR_RELEASE/parm/parm_fv3diag
 export FORECASTSH=$FV3DIR_RELEASE/scripts/exglobal_fcst_nemsfv3gfs.sh         
 
 #---determine task configuration
-export nth_f=1                                         # number of threads 
-export npe_node_f=36                                   # number of pes per node 
+export nth_f=2                                         # number of threads 
+export npe_node_f=24
 export task_per_node=$((npe_node_f/nth_f))
-export tasks=$((NODES*task_per_node))                  # number of tasks 
+export tasks=$((NODES*task_per_node*nth_f))                  # number of tasks 
 export NTHREADS_REMAP=$nth_f
 
 
@@ -111,9 +112,7 @@ module use $( pwd -P )
 module load modules.fv3
 module list
 
-#JLS export mpiexec="mpiexec_mpt"
-#JLS try MPI/OMP hybrid
-export mpiexec="mpiexec_mpt omplace"
+export mpiexec="mpiexec_mpt -n ${tasks}"
 export FCST_LAUNCHER="$mpiexec"
 
 echo "Model started:  " `date`
@@ -128,6 +127,9 @@ $FORECASTSH
 if [ $? != 0 ]; then echo "forecast failed, exit"; exit; fi
 
 echo "fcst job is done"
+
+#JLS Just run the forecast
+exit
 
 #-------------------------------------------------------------------------
 #--convert 6-tile output to global arrary in netCDF format
